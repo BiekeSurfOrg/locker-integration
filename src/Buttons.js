1d4908ec8bfd2a570e634c3bc8ca81aa50b0d0f5
BorisStankov98 <boris.stankovv@gmail.com>
2024-09-24 13:39:45 +0300

    added example call to the locker IP

--- src/Buttons.js
+++ src/Buttons.js
@@ -4,9 +4,9 @@ import Button from './Button';
 function Buttons() {
   return (
     <div className='buttons'>
-      <Button text={"Keyboard"} icon={"keyboard.jpg"} api={'keyboard'}></Button>
-      <Button text={"Mouse"}  icon={"mouse.png"} api={'mouse'}></Button>
-      <Button text={"Headset"}  icon={"headset.png"} api={'headset'}></Button>
+      <Button text={"Keyboard"} icon={"keyboard.jpg"} api={'1'}></Button>
+      <Button text={"Mouse"}  icon={"mouse.png"} api={'2'}></Button>
+      <Button text={"Headset"}  icon={"headset.png"} api={'3'}></Button>
     </div>
   );
 }
--- src/apiCall.js
+++ src/apiCall.js
@@ -1,3 +1,13 @@
+import axios from "axios"
+
 export function callLockedApi(locker){
-    console.log(locker)
+    const apiKey = "example Api key"
+    console.log(`https://lockerip:app-port/api/open/${locker}`, apiKey);
+    
+    axios.post(`https://lockerip:app-port/api/open/${locker}`,{},{
+        headers:{
+                "accept":"*/*",
+                "X-API-KEY":apiKey
+        }
+    }).then((result)=>console.log(result))
 }
