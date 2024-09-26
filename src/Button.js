f8292910ea69fd8e034e97a31acefe262127c138
BorisStankov98 <boris.stankovv@gmail.com>
2024-09-26 15:54:28 +0300

    style changes and added description

--- src/Button.js
+++ src/Button.js
@@ -6,8 +6,8 @@ function Button({ text, icon, lockerId }) {
     return (
         <div>
             <button className='img-button' onClick={() => callLockedApi(lockerId)}>
+                <span className='description'>{text}</span>
                 <img src={require(`./assets/${icon}`)} width={"300px"}></img>
-                <span>{text}</span>
             </button>
         </div>
     );
--- src/Buttons.js
+++ src/Buttons.js
@@ -3,10 +3,15 @@ import Button from './Button';
 
 function Buttons() {
   return (
-    <div className='buttons'>
-      <Button text={"Keyboard"} icon={"keyboard.jpg"} lockerId={'1'}></Button>
-      <Button text={"Mouse"} icon={"mouse.png"} lockerId={'2'}></Button>
-      <Button text={"Headset"} icon={"headset.png"} lockerId={'3'}></Button>
+    <div>
+      <div>
+        <h1 className='header'>What item would you like to loan</h1>
+      </div>
+      <div className='buttons'>
+        <Button text={"Keyboard"} icon={"keyboard.jpg"} lockerId={'1'}></Button>
+        <Button text={"Mouse"} icon={"mouse.png"} lockerId={'2'}></Button>
+        <Button text={"Headset"} icon={"headset.png"} lockerId={'3'}></Button>
+      </div>
     </div>
   );
 }
