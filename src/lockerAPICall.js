c5e4ef2676e3e9e52d03774fa129ff5053accb6d
jd12338 <thomas.rotte@kbc.be>
2024-10-08 14:27:04 +0200

    locker api key

--- src/lockerAPICall.js
+++ src/lockerAPICall.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export function callLockedApi(lockerId) {
-    const URL = "https://lockerip:app-port/api/open/"
-    const apiKey = "example Api key"
+    const URL = "http://10.11.12.235/api/open/"
+    const apiKey = "azk_42163e54-49d8-49b3-89c6-96a734e73daa"
 
     console.log(URL + lockerId, "plus test", apiKey);
 
