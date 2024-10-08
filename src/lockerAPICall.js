d04dd0e7a753dcb9fc1ae1408759f5278f7081eb
jd12338 <thomas.rotte@kbc.be>
2024-10-08 14:38:13 +0200

    locker api key

--- src/lockerAPICall.js
+++ src/lockerAPICall.js
@@ -4,12 +4,21 @@ export function callLockedApi(lockerId) {
     const URL = "http://10.11.12.235/api/open/"
     const apiKey = "azk_42163e54-49d8-49b3-89c6-96a734e73daa"
 
-    console.log(URL + lockerId, "plus test", apiKey);
+//    console.log(URL + lockerId, "plus test", apiKey);
 
-    axios.post(URL + lockerId, {}, {
-        headers: {
-            "accept": "*/*",
-            "X-API-KEY": apiKey
-        }
-    }).then((result) => console.log(result))
+//    axios.post(URL + lockerId, {}, {
+//        headers: {
+//            "accept": "*/*",
+//            "X-API-KEY": apiKey
+//        }
+//    }).then((result) => console.log(result))
+
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://10.11.12.235/api/open/" + lockerId, true);
+    xhr.setRequestHeader('accept', '*/*');
+    xhr.setRequestHeader('X-API-KEY', apiKey);
+    xhr.send(JSON.stringify({
+    }));
+
 }
