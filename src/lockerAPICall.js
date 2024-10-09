fb919a07cb8a82ee6ecd3778c53abfcc41fa48e7
mafke2004 <134381339+mafke2004@users.noreply.github.com>
2024-10-09 16:12:21 +0200

    Update lockerAPICall.js

--- src/lockerAPICall.js
+++ src/lockerAPICall.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export function callLockedApi(lockerId) {
-    const URL = "http://10.11.12.238:8080/open/"
+    const URL = "http://10.11.12.241:8080/open/"
     const apiKey = "azk_42163e54-49d8-49b3-89c6-96a734e73daa"
 
 //    console.log(URL + lockerId, "plus test", apiKey);
